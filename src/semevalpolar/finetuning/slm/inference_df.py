2a664485ac63e4483e041d6f1b154a5573bc7a32
Atharva Gupta <hephannn@gmail.com>
2025-12-30 15:18:43 +0530

    add script to inference_df directly.

---
+++ src/semevalpolar/finetuning/slm/inference_df.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+from random import randint
+
+from semevalpolar.utils import get_project_root
+from semevalpolar.llm.data_utils import read_dataset, create_submission
+from semevalpolar.finetuning.slm.inference import run_inference_on_df
+
+def main():
+    project_root = get_project_root()
+
+    # Load dataset
+    df = read_dataset(os.path.join(project_root, 'data', 'dev_phase', 'subtask1', 'dev', 'eng.csv'))
+
+    # Set model checkpoint directory
+    model_dir = os.path.join(project_root, "predictions", "finetuning", "checkpoints")
+
+    # Run inference on full dataset
+    predictions = run_inference_on_df(df=df, text_column="text", model_dir=model_dir, batch_size=16)
+
+    # Create and save submission
+    submission = create_submission(df, predictions)
+    submission_path = os.path.join(project_root, 'predictions', 'subtask_1', 'pred_eng.csv')
+    submission.to_csv(submission_path, index=False)
+
+if __name__ == "__main__":
+    main()
